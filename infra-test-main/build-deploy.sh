5a569d656fd1554fa0f3e37e96edde9601ef4c8e
Mario Vinicio Gomes de Oliveira <viniciosoliveira@ibm.com>
2025-04-03 20:36:41 -0300

    Ultimas configs

--- infra-test-main/build-deploy.sh
+++ infra-test-main/build-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t seu-usuario/meuapp:latest .
-docker push seu-usuario/meuapp:latest
+docker build -t vinici0s/meuapp:latest .
+docker push vinici0s/meuapp:latest
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
